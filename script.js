c03875cea3cd827649aa676c00f75e082e3d503a
Sandro sulava <sandrosulava2@gmail.com>
2025-05-19 12:01:56 +0400

    core

---
+++ script.js
@@ -0,0 +1,29 @@
+// todo Sandro sulava / 5.19.25
+
+// ? pirvel rigshi saxels gvars emails da ash mivecit let klasi
+
+let FirstName = "Sandro";
+let LastName = "Sulava";
+const Email = "sulava@gmail.com";
+const PhoneNumber = "555789252";
+let Age = "16";
+let Hobby = "football";
+let isStudent = "true";
+
+// ? shemdeg es let klasebi gamovidzaxet console.log shi rom inspect/console gamogvichdes
+
+console.log(FirstName);
+console.log(LastName);
+console.log(Email);
+console.log(PhoneNumber);
+console.log(Age);
+console.log(Hobby);
+console.log(isStudent);
+
+// ? aq kide weli da hobby shevcvale da gamovidzaxe esenic console logshi
+
+let MyAge = "17";
+let MyHobby = "Basketball";
+
+console.log(MyAge);
+console.log(MyHobby);
